df01b1a9b13d854ae41757ac9b404e1d96ef8f7a
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-22 12:52:22 +0900

    Creating JS Files & Add GoogleFonts

---
+++ src/Component/Landing/LandingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./LandingPage.css";
+
+export default class LandingPage extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="card-panel">
+          <span>
+            <h3 className="teal-text">Han Saem's Portfolio</h3>
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
--- src/Component/Layout/Layout.js
+++ src/Component/Layout/Layout.js
@@ -1,14 +1,46 @@
 import React from "react";
+import { About, Skill, Projects, Contact } from "../Nav/index";
+import LandingPage from "./../Landing/LandingPage";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 export default class Layout extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello React World</h1>
+      <Router>
         <div>
-          <h3>This is pofol Site</h3>
+          <nav>
+            <div className="nav-wrapper">
+              <div
+                className="brand-logo"
+                style={{ marginLeft: "auto", paddingLeft: "1em" }}
+              >
+                <Link to="/">HanSame's Pofol</Link>
+              </div>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/skill">Skill</Link>
+                </li>
+                <li>
+                  <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div className="container">
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/about" component={About} />
+            <Route path="/skill" component={Skill} />
+            <Route path="/Projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
