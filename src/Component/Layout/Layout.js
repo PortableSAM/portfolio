6b423c94432e2a066196af97512752d6ada1c1e1
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-23 23:16:54 +0900

    MaterializeCSS에서Bootstrap전환 시작

--- src/Component/Layout/Layout.js
+++ src/Component/Layout/Layout.js
@@ -2,33 +2,30 @@ import React from "react";
 import { About, Projects, Contact } from "../Nav/index";
 import LandingPage from "./../Landing/LandingPage";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import "./Layout.css";
 
 export default class Layout extends React.Component {
   render() {
     return (
       <Router>
         <div>
-          <nav>
-            <div className="nav-wrapper deep-purple lighten-4">
-              <div
-                className="brand-logo"
-                style={{ marginLeft: "auto", paddingLeft: "1em" }}
-              >
-                <Link to="/">HanSame's Pofol</Link>
-              </div>
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-                <li>
-                  <Link to="/projects">Projects</Link>
-                </li>
-                <li style={{ marginRight: "20px" }}>
-                  <Link to="/contact">Contact</Link>
-                </li>
-              </ul>
+          <nav className="nav-wrapper deep-purple lighten-4">
+            <div className="brand-logo">
+              <Link to="/">HanSame's Pofol</Link>
             </div>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+              <li style={{ marginRight: "20px" }}>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
           </nav>
+
           <div className="container">
             <Route exact path="/" component={LandingPage} />
             <Route path="/about" component={About} />
