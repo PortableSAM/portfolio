71877800afe65d9ac8bac9d70aeeda7250423d1c
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-23 22:28:53 +0900

    Component Creation Finish

--- src/Component/Nav/Contact.js
+++ src/Component/Nav/Contact.js
@@ -1,10 +1,34 @@
 import React from "react";
+import "./CSS/Contact.css";
+import img from "./About/Img/img";
 
 export default class Contact extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Contact Page</h2>
+      <div className="contactPage">
+        <div className="title">
+          <h2>Contact</h2>
+          <hr className="contact-hr" style={{ width: "50%" }} />
+        </div>
+        <div className="contact-content">
+          <div className="col s12 m6 imgbox">
+            <img src={img} alt="img" />
+          </div>
+          <div className="content-item">
+            <h4>
+              <i className="fas fa-envelope-square" />
+              hanmti00@naver.com
+            </h4>
+            <h4>
+              <i className="fas fa-comment-dots" />
+              KakoTalk : portableSAM
+            </h4>
+            <h4>
+              <i className="fab fa-facebook-messenger" />
+              hanSaemLicht
+            </h4>
+          </div>
+        </div>
       </div>
     );
   }
