f18599561c643f2bf2166379a75cfe50aae43656
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-24 12:15:50 +0900

    Component Creating

--- src/Component/Layout/Layout.js
+++ src/Component/Layout/Layout.js
@@ -1,37 +1,50 @@
 import React from "react";
-import { About, Projects, Contact } from "../Nav/index";
+import { About, Contact } from "../Nav/index";
 import LandingPage from "./../Landing/LandingPage";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import "./Layout.css";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 export default class Layout extends React.Component {
   render() {
     return (
       <Router>
         <div>
-          <nav>
-            <div>
-              <Link to="/">HanSame's Pofol</Link>
-            </div>
-            <ul>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/projects">Projects</Link>
-              </li>
-              <li style={{ marginRight: "20px" }}>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <div>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand>
+              <Link className="navbar-brand" to="/">
+                Han Saem's Pofol
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+                <NavDropdown title="Project" id="collasible-nav-dropdown">
+                  <div className="drop-menu">
+                    <Link to="/cwa">Current Weather App</Link>
+                    <NavDropdown.Divider />
+                    <Link to="/clock">Clock</Link>
+                    <NavDropdown.Divider />
+                    <Link to="/movielist">Movie List</Link>
+                  </div>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Switch>
             <Route exact path="/" component={LandingPage} />
             <Route path="/about" component={About} />
-            <Route path="/Projects" component={Projects} />
             <Route path="/contact" component={Contact} />
-          </div>
+            <Route path="/cwa" component={CurrentWthrApp} />
+            <Route path="/clock" component={Clock} />
+            <Route path="/movielist" component={MovieList} />
+          </Switch>
         </div>
       </Router>
     );
