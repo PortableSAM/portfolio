bf6f1f2a0d6005a3e8c8c3a3293ada2e93ba4a9d
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-25 19:25:32 +0900

    Add Project

--- src/Component/Nav/Project/MovieList.js
+++ src/Component/Nav/Project/MovieList.js
@@ -1,11 +1,69 @@
 import React from "react";
+import { Jumbotron } from "react-bootstrap";
+import "./Clock.css";
 
-export default class MovieList extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>MovieList</h2>
+function CurrentWthrApp() {
+  return (
+    <div className="clock-container">
+      <div className="clock-title">
+        <h2>Movie List</h2>
+        <hr />
       </div>
-    );
-  }
+      <div className="clock-content">
+        <Jumbotron
+          style={{
+            margin: "auto",
+            display: "flex",
+            padding: "5px",
+            justifyContent: "space-around"
+          }}
+        >
+          <div className="clock-text">
+            <h4>Development environment</h4>
+            <ul>
+              <li>- OS: Windows</li>
+              <li>- IDE: VS Code</li>
+              <li>- Language: JavaScript</li>
+              <li>- Library: React.JS</li>
+            </ul>
+          </div>
+          <div className="clock-text">
+            <h4>Use Open API</h4>
+            <ul>
+              <li>- Kobis Open API</li>
+            </ul>
+          </div>
+          <div className="clock-text">
+            <h4>Development Platform</h4>
+            <ul>
+              <li>- Firebase</li>
+            </ul>
+          </div>
+        </Jumbotron>
+        <hr />
+        <div className="link-icon">
+          <p>
+            <a
+              href="https://movielistapp-b39ef.firebaseapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fas fa-link" />
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://github.com/PortableSAM/Movie_App"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github-square" />
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default CurrentWthrApp;
