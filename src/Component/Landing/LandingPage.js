a39fefbbb370451e5665c4b0d1208c5d34d3bcd8
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-27 19:39:58 +0900

    Modification LandingPage CSS

--- src/Component/Landing/LandingPage.js
+++ src/Component/Landing/LandingPage.js
@@ -1,26 +1,32 @@
 import React from "react";
-//import "./LandingPage.css";
-import { Container } from "react-bootstrap";
+import "./LandingPage.css";
+import { Container, Col, Row } from "react-bootstrap";
 
 export default class LandingPage extends React.Component {
   render() {
     return (
-      <div className="land-body">
+      <>
         <Container>
-          <div className="land">
-            <span>
-              <h3>Han Saem's Portfolio</h3>
-            </span>
-            <hr style={{ marginTop: "20px", marginBottom: "20px" }} />
-            <div className="skill-icon">
-              <i className="fab fa-html5" style={{ fontSize: "4em" }} />
-              <i className="fab fa-css3-alt" style={{ fontSize: "4em" }} />
-              <i className="fab fa-js-square" style={{ fontSize: "4em" }} />
-              <i className="fab fa-react" style={{ fontSize: "4em" }} />
-            </div>
-          </div>
+          <Row>
+            <Col></Col>
+            <Col lg={"8"} sm={"auto"}>
+              <div className="land ">
+                <span>
+                  <h3>Han Saem's Portfolio</h3>
+                </span>
+                <hr style={{ marginTop: "10px", marginBottom: "10px" }} />
+                <div className="skill-icon">
+                  <i className="fab fa-html5" />
+                  <i className="fab fa-css3-alt" />
+                  <i className="fab fa-js-square" />
+                  <i className="fab fa-react" />
+                </div>
+              </div>
+            </Col>
+            <Col></Col>
+          </Row>
         </Container>
-      </div>
+      </>
     );
   }
 }
